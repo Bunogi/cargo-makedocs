57dd5f07af4f92c6a18da728df35e242f40b02fd
Bunogi <2bunogi@gmail.com>
2019-06-05 17:28:56 +0200

    Support workspaces

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,21 @@ use std::env;
 use std::fmt;
 use std::fs::File;
 use std::io::Read;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::{exit, Command};
 use toml::value::{self, Value};
+
+#[derive(Deserialize)]
+struct Workspace {
+    members: Vec<String>,
+}
 
 #[derive(Deserialize)]
 struct CargoToml {
     dependencies: Option<value::Table>,
     #[serde(rename = "build-dependencies")]
     build_dependencies: Option<value::Table>,
+    workspace: Option<Workspace>,
 }
 
 #[derive(Deserialize)]
@@ -73,22 +79,20 @@ fn correct_version<'a>(lock: &'a CargoLock, name: &str, version: &str) -> String
 }
 
 fn get_crates(
-    toml_file: &str,
-    lock_file: &str,
+    manifest: CargoToml,
+    manifest_lock: &CargoLock,
     excluded_crates: &[&str],
     extra_crates: &[&str],
     buildtime: bool,
 ) -> Result<Vec<String>, String> {
-    let root: CargoToml = toml::from_str(toml_file).unwrap();
-    let lock: CargoLock = toml::from_str(lock_file).unwrap();
-    Ok(root
+    Ok(manifest
         .dependencies
         .iter()
         .flatten()
         .chain(
             //Include or ignore buildtime dependencies
             if buildtime {
-                root.build_dependencies
+                manifest.build_dependencies
             } else {
                 None
             }
@@ -127,7 +131,11 @@ fn get_crates(
                 };
 
                 //Get the compatible version from Cargo.lock to always build the correct version
-                Some(correct_version(&lock, changed_name.unwrap_or(k), &version))
+                Some(correct_version(
+                    &manifest_lock,
+                    changed_name.unwrap_or(k),
+                    &version,
+                ))
             } else {
                 None
             }
@@ -162,6 +170,37 @@ fn find_rootdir() -> Result<PathBuf, String> {
     }
 }
 
+//Attempts to find Cargo.lock in starting_dir and the parent dir and load it to a String.
+fn read_cargo_lock(starting_dir: &Path) -> Result<CargoLock, String> {
+    let mut lock_file = String::new();
+    match File::open(starting_dir.join("Cargo.lock")) {
+        Ok(mut f) => {
+            f.read_to_string(&mut lock_file).unwrap();
+        }
+        Err(e) => {
+            //Try the parent directory as if starting_dir is inside a workspace.
+            if let Some(parent) = starting_dir.parent() {
+                match File::open(parent.join("Cargo.lock")) {
+                    Ok(mut f) => {
+                        f.read_to_string(&mut lock_file).unwrap();
+                    }
+                    Err(e) => {
+                        return Err(format!(
+                            "Expected Cargo.lock in workspace dir {} but couldn't open it: {}",
+                            parent.to_string_lossy(),
+                            e
+                        ))
+                    }
+                }
+            } else {
+                return Err(format!("Couldn't open Cargo.lock: {}", e));
+            }
+        }
+    }
+
+    toml::from_str(&lock_file).map_err(|e| format!("Lock file is invalid: {}", e))
+}
+
 fn run(matches: &clap::ArgMatches) -> Result<(), String> {
     let excluded_crates: Vec<&str> = match matches.values_of("exclude") {
         Some(ex) => ex.collect(),
@@ -185,55 +224,108 @@ fn run(matches: &clap::ArgMatches) -> Result<(), String> {
         .read_to_string(&mut cargo_toml)
         .unwrap();
 
-    let mut lock_file = String::new();
-    File::open(dir.join("Cargo.lock"))
-        .map_err(|e| format!("Couldn't open Cargo.lock: {}", e))?
-        .read_to_string(&mut lock_file)
-        .unwrap();
+    let manifest_lock = read_cargo_lock(&dir)?;
+    let manifest: CargoToml = toml::from_str(&cargo_toml)
+        .map_err(|e| format!("{:?} failed to parse: {}", dir.canonicalize().unwrap(), e))?;
 
-    let crates = get_crates(
-        &cargo_toml,
-        &lock_file,
-        &excluded_crates,
-        &extra_crates,
-        !matches.is_present("no-buildtime"),
-    )?;
+    let mut crate_operations = Vec::new(); //`cargo doc`'s -p argument doesn't work when used at the root
+                                           //of a workspace, so queue up operations
+    if let Some(workspace) = manifest.workspace {
+        for member in workspace.members {
+            //Cargo doesn't care about the actual name of a workspaced crate, just it's path
+            let local_dir = dir.join(&member);
 
-    //Build command
-    let mut command = Command::new("cargo");
-    command
-        .arg("doc")
-        .arg("--no-deps")
-        .args(&create_arguments(&crates));
+            let mut local_manifest_contents = String::new();
+            File::open(local_dir.join("Cargo.toml"))
+                .map_err(|e| format!("Couldn't open {:?}: {}", dir.canonicalize().unwrap(), e))?
+                .read_to_string(&mut local_manifest_contents)
+                .unwrap();
 
-    if matches.is_present("document-private-items") {
-        command.arg("--document-private-items");
+            let local_manifest = toml::from_str(&local_manifest_contents)
+                .map_err(|e| format!("{:?} failed to parse: {}", dir.canonicalize().unwrap(), e))?;
+
+            let gotten_crates = get_crates(
+                local_manifest,
+                &manifest_lock,
+                &excluded_crates,
+                &extra_crates,
+                !matches.is_present("no-buildtime"),
+            )?;
+            crate_operations.push((local_dir, gotten_crates)); //Queue up operation
+        }
+    } else {
+        //Sigular crate, only one operation
+        crate_operations.push((
+            dir,
+            get_crates(
+                manifest,
+                &manifest_lock,
+                &excluded_crates,
+                &extra_crates,
+                !matches.is_present("no-buildtime"),
+            )?,
+        ));
     }
 
-    if matches.is_present("root") {
-        let mut pkg_id_command = Command::new("cargo");
-        pkg_id_command.arg("pkgid");
-        let pkg_id =
-            String::from_utf8_lossy(&pkg_id_command.output().unwrap().stdout).replace("\n", "");
-        command.arg("-p").arg(pkg_id);
-    }
+    for (dir, operation) in &crate_operations {
+        //Build command
+        if operation.is_empty() {
+            eprintln!(
+                "cargo-makedocs: no crates to document for dir {}",
+                dir.to_string_lossy()
+            );
+            continue;
+        }
 
-    if crates.is_empty() {
-        return Err("Found no crates to document".into());
-    }
+        let mut command = Command::new("cargo");
+        command
+            .arg("doc")
+            .arg("--no-deps")
+            .args(&create_arguments(&operation));
 
-    //Build documentation
-    command.spawn().unwrap().wait().unwrap();
+        if matches.is_present("document-private-items") {
+            command.arg("--document-private-items");
+        }
+
+        //`cargo doc` does not support the `-p` argument when at the root of a workspace.
+        std::env::set_current_dir(dir).map_err(|e| {
+            format!(
+                "Couldn't switch dir to {}: {}",
+                dir.canonicalize().unwrap().to_string_lossy(),
+                e
+            )
+        })?;
+
+        if matches.is_present("root") {
+            let mut pkg_id_command = Command::new("cargo");
+            pkg_id_command.arg("pkgid");
+            let pkg_id =
+                String::from_utf8_lossy(&pkg_id_command.output().unwrap().stdout).replace("\n", "");
+            command.arg("-p").arg(pkg_id);
+        }
+
+        //Build documentation
+        command.spawn().unwrap().wait().unwrap();
+    }
 
     //Open docs if requested. `cargo doc` doesn't allow --open with more than one -p argument, so
     //it has to be run a second time for this.
-    if matches.is_present("open") {
+    if matches.is_present("open") && !crate_operations[0].1.is_empty() {
         let mut command = Command::new("cargo");
         command.arg("doc").arg("--no-deps").arg("--open");
 
         if !matches.is_present("root") {
-            command.arg("-p").arg(&crates[1]);
+            command.arg("-p").arg(&crate_operations[0].1[0]);
         }
+
+        let dir = &crate_operations[0].0;
+        std::env::set_current_dir(dir).map_err(|e| {
+            format!(
+                "Couldn't switch dir to {}: {}",
+                dir.canonicalize().unwrap().to_string_lossy(),
+                e
+            )
+        })?;
 
         command.spawn().unwrap().wait().unwrap();
     }
@@ -268,7 +360,7 @@ fn main() {
                 Arg::with_name("root")
                     .short("r")
                     .long("root")
-                    .help("Build the documentation for the root crate")
+                    .help("Build the documentation for the root crate. When running in the root of a workspace, document each crate in the workspace as well.")
             ).arg(
                 Arg::with_name("document-private-items")
                     .short("d")
@@ -304,7 +396,9 @@ mod tests {
         let cargo_lock = r#"[[package]]
 name="foo"
 version="1.3.5""#;
-        let crates = get_crates(cargo_toml, cargo_lock, &[], &[], true).unwrap();
+        let manifest = toml::from_str(cargo_toml).unwrap();
+        let manifest_lock = toml::from_str(cargo_lock).unwrap();
+        let crates = get_crates(manifest, &manifest_lock, &[], &[], true).unwrap();
         assert_eq!(crates, ["foo:1.3.5"]);
     }
     #[test]
@@ -320,9 +414,11 @@ version="1.3.5"
 [[package]]
 name = "include-me"
 version="1.2.3""#;
+        let manifest = toml::from_str(cargo_toml).unwrap();
+        let manifest_lock = toml::from_str(cargo_lock).unwrap();
         let crates = get_crates(
-            cargo_toml,
-            &cargo_lock,
+            manifest,
+            &manifest_lock,
             &["some-crate"],
             &["include-me"],
             true,
@@ -341,7 +437,9 @@ version="1.3.2"
 [[package]]
 name = "some-crate"
 version = "1.3.6""#;
-        let crates = get_crates(cargo_toml, cargo_lock, &[], &[], true).unwrap();
+        let manifest = toml::from_str(cargo_toml).unwrap();
+        let manifest_lock = toml::from_str(cargo_lock).unwrap();
+        let crates = get_crates(manifest, &manifest_lock, &[], &[], true).unwrap();
         assert_eq!(crates, ["some-crate:1.3.6"]);
     }
 
@@ -354,7 +452,9 @@ name = "libc"
 version = "0.2.43"
 source = "git+https://github.com/rust-lang/libc#9c5e70ae306463a23ec02179ac2c9fe05c3fb44e"
 "#;
-        let crates = get_crates(cargo_toml, cargo_lock, &[], &[], true).unwrap();
+        let manifest = toml::from_str(cargo_toml).unwrap();
+        let manifest_lock = toml::from_str(cargo_lock).unwrap();
+        let crates = get_crates(manifest, &manifest_lock, &[], &[], true).unwrap();
         assert_eq!(crates, ["libc:0.2.43"]);
     }
 }
